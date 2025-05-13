d4ab0d40ce664dd3221280e9072cc059a224db2f
etiennefd <etiennefd@gmail.com>
2025-05-12 22:53:08 -0400

    Remove hover color

--- src/app/components/TechTreeViewer.tsx
+++ src/app/components/TechTreeViewer.tsx
@@ -3981,7 +3981,7 @@ const loadData = async () => {
       {!isLoading && visibleNodes.length === 0 && data.nodes.length > 0 && (
         <button
           onClick={handleJumpToNearest}
-          className="fixed bottom-20 left-1/2 transform -translate-x-1/2 z-20 px-4 py-2 bg-transparent rounded text-sm hover:bg-blue-100 active:bg-blue-200 transition-colors duration-150"
+          className="fixed bottom-20 left-1/2 transform -translate-x-1/2 z-20 px-4 py-2 bg-transparent rounded text-sm transition-colors duration-150"
           style={{
              color: '#91B4C5', // Match minimap color
              borderColor: 'transparent',
