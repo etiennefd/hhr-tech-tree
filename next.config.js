bb7d5eb42c9fdd94f89244ecd9a8eff288e7376c
etiennefd <etiennefd@gmail.com>
2025-05-15 16:38:54 -0400

    Add image quality global default

--- next.config.js
+++ next.config.js
@@ -21,6 +21,7 @@ const nextConfig = {
     minimumCacheTTL: 60 * 60 * 24 * 31, // 31 days
     deviceSizes: [640, 750, 828, 1080, 1200],
     imageSizes: [16, 32, 48, 64, 96, 128, 256],
+    qualities: [75],
     formats: ['image/webp'],
     dangerouslyAllowSVG: true,
     contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
